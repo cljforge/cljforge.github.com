756dca4026f0e22ef81ecb368c152fa0d66c8e29
m3mfis <noizejke@gmail.com>
2015-10-02 18:06:46 +0300

    state animation

--- assets/js/application/controllers/wrapperCtrl.js
+++ assets/js/application/controllers/wrapperCtrl.js
@@ -1,28 +1,33 @@
-function wrapperCtrl ($scope, $state,$rootScope) {
-    $scope.toggle = function(){
+function wrapperCtrl($scope, $state, $rootScope) {
+    $scope.toggle = function () {
         $rootScope.sidebarOpened = !$scope.sidebarOpened
     };
 
-    $scope.go = function(state){
-        $scope.toggle();
+    $scope.go = function (state) {
         var flasher = $('#flasher');
-
-        //flasher
-        //    .transition({
-        //        width : '2000',
-        //        height : '2000',
-        //        'margin-top' : '-1000px',
-        //        right : '-1000px'
-        //
-        //    },
-        //    function(element){
-        //        $state.go(state)
-        //            .then(function(state){
-        //                flasher.transition({opacity: 0});
-        //            });
-        //    })
+        flasher
+            .transition({
+                'display': 'block'
+            })
+            .transition({
+                'opacity': '1'
+            },
+            'fast'
+            ,
+            function (element) {
+                $scope.toggle();
+                $state.go(state)
+                    .then(function (state) {
+                        flasher
+                            .transition({
+                                opacity: 0
+                            },{
+                                display: 'none'
+                            });
+                    });
+            })
     }
 }
 
-wrapperCtrl['$inject'] = ['$scope','$state','$rootScope'];
-angular.module('clj-forge').controller('wrapperCtrl',wrapperCtrl);
+wrapperCtrl['$inject'] = ['$scope', '$state', '$rootScope'];
+angular.module('clj-forge').controller('wrapperCtrl', wrapperCtrl);
