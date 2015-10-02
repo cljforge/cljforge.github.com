29b2094622f6f6ea182342a19b81aaab7f627121
m3mfis <noizejke@gmail.com>
2015-10-02 17:49:49 +0300

    fixes

--- assets/js/application/controllers/wrapperCtrl.js
+++ assets/js/application/controllers/wrapperCtrl.js
@@ -1,6 +1,26 @@
 function wrapperCtrl ($scope, $state,$rootScope) {
     $scope.toggle = function(){
         $rootScope.sidebarOpened = !$scope.sidebarOpened
+    };
+
+    $scope.go = function(state){
+        $scope.toggle();
+        var flasher = $('#flasher');
+
+        //flasher
+        //    .transition({
+        //        width : '2000',
+        //        height : '2000',
+        //        'margin-top' : '-1000px',
+        //        right : '-1000px'
+        //
+        //    },
+        //    function(element){
+        //        $state.go(state)
+        //            .then(function(state){
+        //                flasher.transition({opacity: 0});
+        //            });
+        //    })
     }
 }
 
